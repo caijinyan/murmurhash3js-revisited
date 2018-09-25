a897dc264a57da672ee7a92ecb4482006a32b98e
Alex Ciminian <alex.ciminian@gmail.com>
2018-09-26 00:46:25 +0100

    Implement dynamic comparison table between wasm and JS implementations

--- src/App.js
+++ src/App.js
@@ -2,40 +2,68 @@ import React, { Component } from 'react';
 import 'hack';
 import logo from './logo.svg';
 
+const notImplemented = () => "N/A";
+
+const Result = ({ variant, input, hashes }) => {
+  const hashFns = Object.keys(hashes).map(name => hashes[name][variant] || notImplemented);
+  const outputs = hashFns.map(hashFn => hashFn(input));
+  const correct = output => (output + "") === (outputs[0] + "");
+  const format = output => output.length === 32 ? <span>{output.slice(0, 16)}<wbr />{output.slice(16)}</span> : output;
+  return (
+    <tr>
+      <td className="input">{input}</td>
+      {outputs.map((output, idx) =>
+        (<td key={idx} className={correct(output) ? 'success' : 'failure'}>{format(output)}</td>))}
+    </tr>
+  );
+};
+
+const Heading = ({ variant, hashes }) => {
+  return (
+    <thead>
+      <tr>
+        <th>{variant}</th>
+        {Object.keys(hashes).map(name => <th key={name}>{name}</th>)}
+      </tr>
+    </thead>
+  );
+}
+
+// order of the names must be consistent
+// name, variant => hashes[name][variant]
+// variant, name => hashes[variant][name]
+const Table = ({ hashes, variant, inputs }) => {
+  return (
+    <table>
+      <Heading hashes={hashes} variant={variant} />
+      <tbody>
+        {inputs.map(input => <Result hashes={hashes} variant={variant} input={input} />)}
+      </tbody>
+    </table>
+  );
+};
+
+const shortTests = [
+  "My hovercraft is full of eels.",
+  "My 🚀 is full of 🦎.",
+  "吉 星 高 照",
+  "🌈 utf-8"
+];
+
+const allTests = [
+  "I will not buy this record, it is scratched.",
+  "I will not buy this tobaconnists, it is scratched.",
+].concat(shortTests);
+
 class App extends Component {
   render() {
+    const { hashes } = this.props;
     return (
       <div className="container">
-      <table>
-        <thead>
-        <tr>
-        <th>editor</th>
-        <th>speed</th>
-        <th>extension</th>
-        <th>interface</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-        <td>sublime</td>
-        <td>90</td>
-        <td>80</td>
-        <td>70</td>
-        </tr>
-        <tr>
-        <td>atom</td>
-        <td>60</td>
-        <td>85</td>
-        <td>80</td>
-        </tr>
-        <tr>
-        <td>vscode</td>
-        <td>80</td>
-        <td>65</td>
-        <td>60</td>
-        </tr>
-        </tbody>
-      </table>
+        <Table hashes={hashes} variant="x86  32bit" inputs={shortTests} />
+        {/* the long tests cases are not used in the first table for aesthetic reasons */}
+        <Table hashes={hashes} variant="x86 128bit" inputs={allTests} />
+        <Table hashes={hashes} variant="x64 128bit" inputs={allTests} />
         <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import murmurhash3jsModified from "@cimi/murmurhash3js";
 import App from "./App";
 import './index.css';
 import WasmLoader from "./SMHasher";
-import { validator } from "./validator";
 
 function bufferToHex(buffer) {
   return Array
@@ -18,31 +17,29 @@ function bufferToHex(buffer) {
 window.onload = () => {
   const module = WasmLoader({ locateFile: () => '/SMHasher.wasm' });
 
-  const ref_x86_32 = module.cwrap('MurmurHash3_x86_32_reference', 'string', ['string']);
-  const ref_x86_128 = module.cwrap('MurmurHash3_x86_128_reference', 'string', ['string']);
-  const ref_x64_128 = module.cwrap('MurmurHash3_x64_128_reference', 'string', ['string']);
+  const bytes = str => new TextEncoder().encode(str);
 
-  // const hashToTest = (input) => bufferToHex(murmur128(input));
-  // const reference = murmurhash3js.x86.hash128;
+  const hashes = {
+    "C++ reference": {
+      "x86  32bit": module.cwrap('MurmurHash3_x86_32_reference', 'string', ['string']),
+      "x86 128bit": module.cwrap('MurmurHash3_x86_128_reference', 'string', ['string']),
+      "x64 128bit": module.cwrap('MurmurHash3_x64_128_reference', 'string', ['string'])
+    },
+    murmurhash3js: {
+      "x86  32bit": murmurhash3jsOriginal.x86.hash32,
+      "x86 128bit": murmurhash3jsOriginal.x86.hash128,
+      "x64 128bit": murmurhash3jsOriginal.x64.hash128
+    },
+    "@cimi/murmurhash3js": {
+      "x86  32bit": str => murmurhash3jsModified.x86.hash32(bytes(str)),
+      "x86 128bit": str => murmurhash3jsModified.x86.hash128(bytes(str)),
+      "x64 128bit": str => murmurhash3jsModified.x64.hash128(bytes(str))
+    }
+  };
 
-  const hashToTest = str => {
-    const bytes = new TextEncoder().encode(str);
-    // console.log('JS input bytes:', bufferToHex(bytes));
-    return murmurhash3jsModified.x86.hash128(bytes);
-  }
-
-  const reference = ref_x86_128;
-
-  const verify = validator(hashToTest, reference);
   module.onRuntimeInitialized = () => {
-    verify("0");
-    verify("I will not buy this record, it is scratched.");
-    verify("I will not buy this tobacconist's, it is scratched.");
-    verify("My hovercraft is full of eels.");
-    verify("utf-8 supported 🌈");
-    verify("👻☠️💩");
-    verify("這個有效");
+    ReactDOM.render(<App hashes={hashes} />, document.getElementById('root'));
   }
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
