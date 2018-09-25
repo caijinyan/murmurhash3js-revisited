049b8e7253c4087113a02ba96d1eb2c81038bfff
Alex Ciminian <alex.ciminian@gmail.com>
2018-09-25 23:09:38 +0100

    Add hack stylesheet, remove default cra css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import 'hack';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div className="container">
+      <table>
+        <thead>
+        <tr>
+        <th>editor</th>
+        <th>speed</th>
+        <th>extension</th>
+        <th>interface</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>sublime</td>
+        <td>90</td>
+        <td>80</td>
+        <td>70</td>
+        </tr>
+        <tr>
+        <td>atom</td>
+        <td>60</td>
+        <td>85</td>
+        <td>80</td>
+        </tr>
+        <tr>
+        <td>vscode</td>
+        <td>80</td>
+        <td>65</td>
+        <td>60</td>
+        </tr>
+        </tbody>
+      </table>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
