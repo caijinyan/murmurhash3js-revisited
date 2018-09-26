726fdd9cc1e93563caa108b4abede2fa4f4a3a0a
Alex Ciminian <alex.ciminian@gmail.com>
2018-09-26 17:56:41 +0100

    Update usage examples with how to disable validation

--- src/code-samples/usage.js
+++ src/code-samples/usage.js
@@ -7,20 +7,30 @@ MurmurHash3.x86.hash32(bytes);
 // output: 2953494853
 
 MurmurHash3.x86.hash128(bytes);
-// output: e3a186aee169ba6c6a8bd9343c68fa9c
+// output: "e3a186aee169ba6c6a8bd9343c68fa9c"
 
 MurmurHash3.x64.hash128(bytes);
-// output: 03e5e14d358c16d1e5ae86df7ed5cfcb
+// output: "03e5e14d358c16d1e5ae86df7ed5cfcb"
 
 MurmurHash3.x86.hash32("any string");
 // output: undefined
 // (x86.hash128 and x64.hash128 also return undefined)
 
 MurmurHash3.x86.hash32(["a", "b", "c"]);
+MurmurHash3.x86.hash32([256]);
+MurmurHash3.x86.hash32([3, 133, "foo"]);
 // output: undefined
 // (x86.hash128 and x64.hash128 also return undefined)
 
-MurmurHash3.x86.hash32(anyOtherInvalidInput);
-// output: undefined
-// (x86.hash128 and x64.hash128 also return undefined)
+// since validating the input adds overhead you can disable it
+// this assumes you trust your input, string will always yield wrong results!
+MurmurHash3.inputValidation = false;
+
+MurmurHash3.x86.hash32("this now returns wrong results")
+MurmurHash3.x86.hash128(["as", "does", "this", "and", "the", "next", "one"]);
+MurmurHash3.x64.hash128([1234, 5678, 9999]);
+// output: incorrect hashes
+
+MurmurHash3.x86.hash32(1001);
+// Error!
 `;
